72fb0c4a0a821b5f48cfeb2a2aac404b3ade14bb
menoIy <karim.daou.1993@gmail.com>
2021-08-30 11:33:14 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      <p>Hello There</p>
+    </div>
+  );
+};
+
+export default App;
