f977346734ae97f694a2cc76ecfbf694a00002c3
menoIy <karim.daou.1993@gmail.com>
2021-08-30 15:54:07 +0100

    create Component PokemomList

---
+++ src/components/PokemonsList.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Pokemon from "../components/Pokemon";
+import useFetch from "../hooks/useFetch";
+import { getPokemonsListQuery, getPokemonImgUrl } from "../pokeApi";
+
+type IPokeData = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Array<{ name: string; url: string }>;
+};
+
+type IUseFetch = {
+  data: IPokeData | null;
+  loading: boolean;
+  error: boolean;
+};
+
+const PokemonsList = (): JSX.Element => {
+  const { data, loading, error }: IUseFetch = useFetch(getPokemonsListQuery());
+
+  return (
+    <div>
+      {error && <p>Error</p>}
+      {loading && <p>Loading...</p>}
+      {data && (
+        <div className="pokemon__list">
+          {data.results.map((pokemon, index) => (
+            <Pokemon
+              key={index}
+              id={index}
+              name={pokemon.name}
+              img={getPokemonImgUrl(index + 1)}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PokemonsList;
