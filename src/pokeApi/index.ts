53970fda596581f7f5364857127fa984f642e7f0
menoIy <karim.daou.1993@gmail.com>
2021-08-30 15:53:42 +0100

    create pokeApi helper functions

---
+++ src/pokeApi/index.ts
@@ -0,0 +1,19 @@
+const pokeUrl = "https://pokeapi.co/api/v2";
+const pokeImgUrl =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world";
+
+type GetPokeListOptions = {
+  limit?: number;
+  offset?: number;
+};
+
+export const getPokemonsListQuery = (options?: GetPokeListOptions): string => {
+  const limit = options?.limit || 20;
+  const offset = options?.offset || 0;
+
+  return `${pokeUrl}/pokemon?limit=${limit}&offset=${offset}`;
+};
+
+export const getPokemonImgUrl = (id: number): string => {
+  return `${pokeImgUrl}/${id}.svg`;
+};
