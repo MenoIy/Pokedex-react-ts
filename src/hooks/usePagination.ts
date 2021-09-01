b4d46e9b7a06c6b17764a3e728c1dbd289a19649
menoIy <karim.daou.1993@gmail.com>
2021-09-01 20:44:06 +0100

    fix pagination

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -2,16 +2,20 @@ import { useMemo } from "react";
 
 export const usePagination = (pagesCount: number, currentPage: number) => {
   const getPagesRange = useMemo(() => {
+    const count = pagesCount >= 6 ? 6 : pagesCount;
+
     const getOffset = () => {
       if (currentPage - 2 < 1) return 1;
-      else if (pagesCount - currentPage >= 6) return currentPage - 2;
-      else return currentPage - 5;
+      else if (pagesCount - currentPage >= count) return currentPage - 2;
+      else return currentPage - (count - 1);
     };
 
     const offset = getOffset();
-    const pages: Array<number> = new Array(6)
+
+    const pages: Array<number> = new Array(count)
       .fill(0)
       .map((_, index) => offset + index);
+
     return pages;
   }, [pagesCount, currentPage]);
 
